126bfa38eabc60abf988f19ca4aabe8790be3eee
Yuliia Vorobei <vorobeju1@gmail.com>
2024-05-10 21:11:47 +0200

    js homework 3

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function filterArray(numbers, value) {
+  const newNumbers = [];
+  for (const number of numbers) {
+    if (number > value) {
+      newNumbers.push(number);
+    }
+  }
+  return newNumbers;
+}
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
